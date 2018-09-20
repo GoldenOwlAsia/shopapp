c8996c81c76cacb85c23878d35fe892dc17691f9
Levy <khacvy93@gmail.com>
2018-09-20 16:20:32 +0700

    fix Create Customer Modal

--- app/src/components/NewCustomerModal.js
+++ app/src/components/NewCustomerModal.js
@@ -6,6 +6,8 @@ import {
   Text,
 } from 'react-native';
 import Button from './Button';
+import BaseButton from './BaseButton';
+import TouchableView from './TouchableView';
 import TextInput from './TextInput';
 import { colors } from "../utils/constants";
 
@@ -39,18 +41,26 @@ class NewCustomerModal extends Component {
             </View>
             <View style={styles.createCustomerForm}>
               <TextInput
-                wrapperStyle={styles.textInput}
-                placeholder="Customer's name"
+                label="Tên khách hàng"
                 value={this.state.customerName}
                 onChangeText={this.onChangeCustomerName}
               />
               <TextInput
-                wrapperStyle={styles.textInput}
-                placeholder="Phone number"
+                label="Số điện thoại"
                 value={this.state.customerPhoneNumber}
                 onChangeText={this.onChangeCustomerPhone}
               />
-              <View style={styles.buttonsWrapper}>
+
+              <BaseButton
+                containerStyle={styles.btnSubmit}
+                onPress={this.handleSubmit}
+                fullWidth
+                title={this.props.submitText}
+              />
+              <TouchableView onPress={this.props.onRequestClose}>
+                <Text style={styles.cancelText}>{this.props.cancleText}</Text>
+              </TouchableView>
+              {/* <View style={styles.buttonsWrapper}>
                 <Button
                   text={this.props.submitText}
                   primary
@@ -63,7 +73,7 @@ class NewCustomerModal extends Component {
                   textStyle={{color: 'black'}}
                   onPress={this.props.onRequestClose}
                 />
-              </View>
+              </View> */}
             </View>
           </View>
         </View>
@@ -81,40 +91,41 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     backgroundColor: colors.WHITE,
-    height: 320,
     width: '100%',
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
     justifyContent: 'center',
     flexDirection: 'column'
   },
   modalTitle: {
     borderBottomWidth: 1,
     flexDirection: 'column',
-    borderBottomColor: 'black',
+    borderBottomColor: '#ececec',
     alignItems: 'center',
     justifyContent: 'center',
     height: 70
   },
   modalTitleText: {
-    fontWeight: 'bold',
+    lineHeight: 24,
+    fontFamily: "Rubik-Medium",
     fontSize: 18,
-    marginTop: 15
+    fontWeight: "700",
+    color: "#12283f"
   },
   createCustomerForm: {
     flexDirection: 'column',
-    flexWrap: 'wrap',
-    paddingLeft: 20,
-    paddingRight: 20
+    padding: 20,
   },
-  textInput: {
-    marginTop: 20,
-    height: 50
+  btnSubmit: {
+    marginTop: 12,
   },
-  buttonsWrapper: {
-    marginTop: 20,
-    marginBottom: 20,
-    flexWrap: 'wrap'
+  cancelText: {
+    marginTop: 16,
+    textAlign: 'center',
+    lineHeight: 19,
+    fontFamily: "Rubik-Medium",
+    fontSize: 14,
+    color: "#12283f"
   }
 });
 
