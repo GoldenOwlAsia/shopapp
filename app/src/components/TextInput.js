7aa9e529f30311d839377ab32a4c14d885004189
Levy <khacvy93@gmail.com>
2018-09-20 11:18:25 +0700

    Fix login screen

--- app/src/components/TextInput.js
+++ app/src/components/TextInput.js
@@ -5,16 +5,56 @@ import {
   TextInput,
   KeyboardAvoidingView,
   Text,
+  View,
 } from 'react-native';
 import { PropTypes } from 'prop-types';
+import { TextField } from 'react-native-material-textfield';
 
 const styles = StyleSheet.create({
-  wrapper: {
-    padding: 10,
-    backgroundColor: '#F6F6F8',
+  containerStyle: {
+    borderRadius: 6,
+    backgroundColor: "#f6f6f8",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "#eeeeee",
+    paddingLeft: 16,
+    paddingRight: 16,
   },
-  text: {
-    width: '100%'
+  wrapper: {
+    // padding: 10,
+    // backgroundColor: '#F6F6F8',
+  },
+  container: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 10,
+    paddingBottom: 10,
+    height: 60,
+    borderRadius: 6,
+    backgroundColor: "#f6f6f8",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "#eeeeee"
+  },
+  label: {
+    fontSize: 14,
+    lineHeight: 19,
+    fontWeight: 'normal',
+    color: "#caced4",
+    fontFamily: 'Rubik-Regular'
+  },
+  input: {
+    width: '100%',
+    fontFamily: "Rubik-Regular",
+    fontSize: 16,
+    fontWeight: '600',
+    color: "#12283f",
+  },
+  textInput: {
+    fontFamily: "Rubik-Regular",
+    fontSize: 16,
+    fontWeight: '600',
+    color: "#12283f",
   }
 });
 
@@ -29,17 +69,28 @@ class NormalTextInput extends Component {
     this.props.onChange && this.props.onChange(text);
   }
 
-  render() {
+  render(){
+    const { label, spaceTop, ...rest } = this.props;
     return (
-      <KeyboardAvoidingView style={[styles.wrapper, this.props.wrapperStyle]} behavior="padding" enabled>
-        {!!this.props.label && <Text>{this.props.label}</Text>}
-        <TextInput
-          style={[styles.text, this.props.style]}
-          underlineColorAndroid="transparent"
-          {...this.props}
-        />
-      </KeyboardAvoidingView>
-    );
+      <TextField
+        {...rest}
+        label={label}
+        value={this.state.phone}
+        onChangeText={this.handleChange}
+        containerStyle={[styles.containerStyle, spaceTop && { marginTop: 10 }]}
+        tintColor="#caced4"
+        baseColor="#caced4"
+        labelFontSize={14}
+        titleFontSize={24}
+        fontSize={16}
+        textColor={'#12283f'}
+        lineWidth={0}
+        activeLineWidth={0}
+        style={styles.textInput}
+        autoCapitalize={'none'}
+        value={this.state.text}
+      />
+    )
   }
 }
 
@@ -49,7 +100,11 @@ NormalTextInput.propTypes = {
   wrapperStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
 }
 
+NormalTextInput.defaultProps = {
+  spaceTop: true,
+}
+
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('ShopApp', () => NormalTextInput);
+// AppRegistry.registerComponent('ShopApp', () => NormalTextInput);
 
 export default NormalTextInput;
