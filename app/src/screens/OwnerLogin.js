71228d571df376882461af3aaeae9b8204a25d2a
Levy <khacvy93@gmail.com>
2018-09-24 11:24:39 +0700

    fix owner login screen

--- app/src/screens/OwnerLogin.js
+++ app/src/screens/OwnerLogin.js
@@ -4,9 +4,11 @@ import {
   StyleSheet,
   View,
   Text,
-  TouchableOpacity,
-  TextInput
+  Alert,
+  TextInput,
+  ActivityIndicator,
 } from 'react-native';
+import TouchableView from '../components/TouchableView';
 import { colors } from '../utils/constants';
 
 import { ownerLogin } from '../actions/auth';
@@ -16,91 +18,147 @@ class OwnerLogin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: ['', '', '', '']
+      codeValue: '',
+      index: 0,
+      loading: false,
     }
   }
 
-  onChangeCode = (index, character) => {
-    const code = [...this.state.code];
-    code[index] = character;
-    this.setState({
-      code
+  onChangeText = (text) => {
+    this.setState({ codeValue: text, index: text.length - 1 }, () => {
+      if(this.state.codeValue.length === 4){
+        this.hiddenInput.blur();  
+        setTimeout(() => {
+          this.handleLogin(this.state.codeValue);
+        }, 500);
+      }
     });
-    const emptyField = code.filter((char) => !char && char !== '0').length;
-    if (!emptyField) {
-      this.handleLogin(code);
-    }
   }
 
-  handleLogin = async (codeArr) => {
-    const code = codeArr.join('');
+  onSelect = (index) => {
+    this.hiddenInput.focus();
+  }
+
+  handleLogin = async (code) => {
+    this.setState({ loading: true });
     const result = await this.props.ownerLogin({ code });
+    this.setState({ loading: false });
     console.log('owner login result: ', result);
     if (result.type === OWNER_LOGIN_SUCCESS) {
       this.props.navigation.navigate('Owner');
+    }else {
+      Alert.alert('Error', 'Invalid code');
     }
   }
 
-  renderInputs = () => {
-    return this.state.code.map((value, index) => {
-      return (
-        <TextInput
-          style={[styles.input, value ? styles.hasValue : {}]}
-          keyboardType='numeric'
-          maxLength={1}
-          value={index}
-          underlineColorAndroid="transparent"
-          onChangeText={(text) => this.onChangeCode(index, text)}
-        />
-      )
-    })
-  }
-
   render() {
+    const { codeValue, index, loading } = this.state;
+    if(loading){
+      return (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator animating size="large" />
+        </View>
+      )
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.hint}>
-          We've sent you 4 digital security code. Please enter them here.
+        <Text style={styles.guide}>
+          Chúng tôi đã gửi cho bạn 4 kí tự bảo mật đến
+          số điện thoại của bạn để xác nhận đăng nhập
         </Text>
-
-        <Text style={styles.label}></Text>
-        <View style={styles.verifyCode}>
-          {this.renderInputs()}
+        <Text style={styles.hint}>Nhập mã bảo mật ở đây</Text>
+        <View style={styles.codeContainer}>
+          <CodeElement onSelect={this.onSelect} index={0} value={codeValue.charAt(0)} editing={index === 0} />
+          <CodeElement onSelect={this.onSelect} index={1} value={codeValue.charAt(1)} editing={index === 1} />
+          <CodeElement onSelect={this.onSelect} index={2} value={codeValue.charAt(2)} editing={index === 2} />
+          <CodeElement onSelect={this.onSelect} index={3} value={codeValue.charAt(3)} editing={index === 3} />
         </View>
+        <TextInput value={this.state.codeValue} style={styles.hiddenInput} ref={(ref)=>{this.hiddenInput = ref}} returnKeyType="done" maxLength={4} keyboardType="numeric" onChangeText={this.onChangeText} autoFocus />
+        <Text style={styles.hint}>Không nhận được tin nhắn.</Text>
+        <TouchableView>
+          <Text style={styles.resendText}>Gửi lại!</Text>
+        </TouchableView>
       </View>
     )
   }
 }
 
+const CodeElement = (props) => {
+  const { index, value, editing, onSelect } = props;
+
+  const _onSelect = () => onSelect(index);
+
+  return (
+    <TouchableView onPress={_onSelect}>
+      <View style={[styles.codeWrap, editing && styles.codeWrapEditing]}>
+          <Text>{value}</Text>
+      </View>
+    </TouchableView>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingLeft: 20,
-    paddingRight: 20
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+    backgroundColor: '#FFF',
+  },
+  loadingContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  guide: {
+    marginBottom: 50, 
+    textAlign: 'center',
+    fontFamily: "Rubik-Regular",
+    lineHeight: 22,
+    fontSize: 16,
+    color: "#12283f"
   },
   hint: {
     textAlign: 'center',
-    fontSize: 16,
-    paddingTop: 30,
-    paddingBottom: 30
+    fontFamily: "Rubik-Regular",
+    lineHeight: 24,
+    fontSize: 18,
+    color: "#868686"
   },
-  verifyCode: {
+  codeContainer: {
+    marginTop: 30,
+    marginBottom: 30,
     flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  codeWrap: {
+    width: 60,
+    height: 60,
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  input: {
-    width: 50,
-    height: 50,
+    padding: 20,
+    borderRadius: 4,
     borderWidth: 1,
-    borderColor: colors.GREY_97,
-    backgroundColor: colors.WHITE,
-    marginLeft: 15,
-    marginRight: 15,
-    textAlign: 'center'
+    borderColor: '#F7F7F7',
+    backgroundColor: '#F7F7F7',
   },
-  hasValue: {
-    backgroundColor: colors.GREY_97
+  codeWrapEditing: {
+    borderColor: '#eeeeee',
+    backgroundColor: '#FFF',
+  },
+  resendText: {
+    color: 'green',
+    alignSelf: 'center',
+    padding: 8,
+  },
+  hiddenInput: {
+    width: 0,
+    height: 0,
+    position: 'absolute',
+    left: -999,
   }
 });
 
